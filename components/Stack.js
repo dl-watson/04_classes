f71f50bc3ed8e79297aab31b912f53580da432bb
dl-watson <dlarkinwatson@gmail.com>
2020-12-03 13:35:06 -0800

    initialize basic file architecture, install dependencies

---
+++ components/Stack.js
@@ -0,0 +1,18 @@
+class Stack {
+  #stack;
+
+  constructor() {
+    this.#stack = [];
+  }
+
+  // put item on top of stack
+  push(item) {}
+
+  // take item off the top of stack and return that item
+  pop() {}
+
+  // look at item on top of stack
+  scry() {}
+}
+
+module.exports = Stack;
