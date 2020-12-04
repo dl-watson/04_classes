430a50a0203e7bb2252beee57cd5a36f1fe623bd
dl-watson <dlarkinwatson@gmail.com>
2020-12-04 11:53:03 -0800

    got a working tested refactor

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const app = express();
-const Stack = require("./lib/components/Stack");
+const { evaluate } = require("./lib/components/utils/utils.js");
 
 app.use(express.json());
 
 app.post("/api/v1/stack", (req, res) => {
-  const stack = new Stack(req.body.item);
-  res.send(stack.evaluate());
+  console.log(req.body.item);
+  res.send(evaluate(req.body.item));
 });
 
 app.listen(7890, () => {
